fd95743a27dccab988d7e988453dd5207abc251d
NgCgThanh <thanhhsnvlxd2000@gmail.com>
2026-01-21 21:03:07 +0700

    redeploy

--- src/modules/movies/movies.controller.ts
+++ src/modules/movies/movies.controller.ts
@@ -27,6 +27,7 @@ export class MoviesController {
     return this.service.findAll();
   }
 
+  //
   @Get('/customer')
   @ApiQuery({ name: 'customer_id', required: false, type: String })
   findAllByCustomerId(@Query('customer_id') customer_id?: string) {
